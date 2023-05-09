33e76955b9755036350b7d6601b1529c1ec1395e
forasteyros <86174077+sitessac@users.noreply.github.com>
2023-05-09 20:03:28 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require( 'express' )
+const app = express()
+const path = require( 'path' )
+
+const { join } = path
+
+const port = process.env.PORT || 4000
+const server = require( 'http' ).createServer( app )
+const io = require( 'socket.io' )( server )
+
+app.use( express.static( join( __dirname , 'public' ) ) )
+app.set( 'views' , join( __dirname , 'public' ) )
+app.engine( 'html' , require('ejs').renderFile )
+app.set( 'view engine' , 'html' )
+
+app.use( '/' , ( req , res ) => {
+  res.render( 'index.php' )
+} )
+
+let messages = []
+let connectionsInfo = {
+  connections: 0
+}
+
+io.on( 'connection' , socket => {
+
+  connectionsInfo.connections = server.getConnections( ( err , count ) => {
+    return count
+  } )
+  
+  socket.emit( 'ConnectionsInfo' , connectionsInfo )
+  socket.emit( 'previousMessages' , messages )
+
+  socket.on( 'sendMessage' , data => {
+    messages.push( data )
+    socket.broadcast.emit( 'receivedMessage' , data )
+  } )
+} )
+
+server.listen( port , () => {
+  console.log( `Server running on localhost:${port}` )
+} )
